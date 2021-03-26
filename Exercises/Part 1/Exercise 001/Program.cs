575e7b76e59a560f2277d088eeba2a255e8862a2
JarkTol <jarkko.tolli@centria.fi>
2021-03-26 12:58:57 +0200

    26.3.2021

--- Exercises/Part 1/Exercise 001/Program.cs
+++ Exercises/Part 1/Exercise 001/Program.cs
@@ -6,8 +6,9 @@ namespace exercise_01
     {
         public static void Main(string[] args)
         {
-           
-        Console.WriteLine("Hello world");
+            Console.WriteLine("Hello World!");  //This is basic Hello World!
+
+            Console.ReadLine();      //This is just stopping console window, VS is bugging and no solution after 2 days google.
 
         }
         
