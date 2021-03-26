8b3b111f21040e933afc1851ced1c1e77ae1a43e
JarkTol <jarkko.tolli@centria.fi>
2021-03-26 13:39:56 +0200

    26.3.2021

--- Exercises/Part 1/Exercise 005/Program.cs
+++ Exercises/Part 1/Exercise 005/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_05
         public static void Main(string[] args)
         {
             string name = "Ada Lovelace";
-            // Write your code here:
+            
+            Console.Write("Hello ");
+
+            if (name == "Ada Lovelace");    //if statement changes l letter to L.
+            
+            {Console.Write("Ada LoveLace");}
+            
+            Console.Write("!");
+
+            Console.ReadLine();
             
         }
     }
