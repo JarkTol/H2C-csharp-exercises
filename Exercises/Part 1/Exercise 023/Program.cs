67b327eeda49191c0ca11b3446e7e75498f31dd4
JarkTol <jarkko.tolli@centria.fi>
2021-04-07 17:00:25 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -6,9 +6,22 @@ namespace exercise_23
   {
     public static void Main(string[] args)
     {
+            
+        Console.WriteLine("Give the first number");
+        int First = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Give the second number");
+        int Second = Convert.ToInt32(Console.ReadLine());
+        int Add = First + Second;
+        int Subtract = First - Second;
+        int Multiply = First * Second;
+        double Division = Convert.ToDouble(First) / Convert.ToDouble(Second);
+        Console.WriteLine(First + " + " + Second + " = " + Add);
+        Console.WriteLine(First + " - " + Second + " = " + Subtract);
+        Console.WriteLine(First + " * " + Second + " = " + Multiply);
+        Console.WriteLine(First + " / " + Second + " = " + Division);
+        Console.ReadLine();
+        
+        }
 
-      // Write your code here:
-
-    }
   }
 }
