d9b02977a29d85862f8265a5f34db7d1eb5303e2
JarkTol <jarkko.tolli@centria.fi>
2021-04-07 16:46:46 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -7,7 +7,15 @@ namespace exercise_22
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number");
+            int First = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number");
+            int Second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the third number");
+            int Third = Convert.ToInt32(Console.ReadLine());
+            double Average = (First + Second +Third) / 3.0;
+            Console.WriteLine("The average is " + Average);
+            Console.ReadLine();
 
     }
   }
