f4a9677f0bb6fbcce3cb87834aca83929c56fea6
JarkTol <jarkko.tolli@centria.fi>
2021-04-07 13:52:38 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -7,8 +7,13 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
+            Console.WriteLine("Give the first number");
+            int First = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number");
+            int Second = Convert.ToInt32(Console.ReadLine());
+            int Sum = (First + Second);
+            Console.WriteLine("Sum is " + Sum);
+            Console.ReadLine();
     }
   }
 }
