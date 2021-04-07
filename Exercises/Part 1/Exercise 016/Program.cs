becf2fb3b3bf7e6dce8a51585608724952677aff
JarkTol <jarkko.tolli@centria.fi>
2021-04-07 13:45:18 +0300

    7.4.2021

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -7,7 +7,10 @@ namespace exercise_16
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give days");
+            int Seconds = (Convert.ToInt32(Console.ReadLine())* 60 * 60 * 24);
+            Console.WriteLine(Seconds);
+            Console.ReadLine();
 
     }
   }
