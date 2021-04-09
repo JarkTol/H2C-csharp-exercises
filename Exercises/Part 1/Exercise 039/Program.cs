1bbdde1aeab04919b471ffc83c34038d079971d2
JarkTol <jarkko.tolli@centria.fi>
2021-04-09 20:30:08 +0300

    9.4.2021

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,25 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int Count = 0;
+            
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                int Number = Convert.ToInt32(Console.ReadLine());
+                
+                if (Number != 0)
+                {                       
+                    Count = Count + Number;
+                }
+
+                else
+                {
+                   Console.WriteLine("Total sum of numbers: " + Count);
+                    break;
+                }
+            }
+      Console.ReadLine();
     }
   }
 }
