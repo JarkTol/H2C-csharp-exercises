17ccbfe3e4039bb23b5828c638da95d74f604ce4
JarkTol <jarkko.tolli@centria.fi>
2021-04-09 15:33:08 +0300

    9.4.2021

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,10 +6,21 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-        int Home = Convert.ToInt32((Console.ReadLine);
+        Console.WriteLine("Give a number:");
+            
+            int Number = Convert.ToInt32(Console.ReadLine());
+            
+            if ((Number % 2) == 0)
+            {
+                Console.WriteLine("Number is even.");
+            }
+            
+            else
+            {
+                Console.WriteLine("Number is odd.");
+            }
 
-
-      if 
+            Console.ReadKey();
     }
   }
 }
