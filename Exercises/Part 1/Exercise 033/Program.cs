d14613803b316efcbce01df1fdd504609ffd51e9
JarkTol <jarkko.tolli@centria.fi>
2021-04-09 15:49:23 +0300

    9.4.2021

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,8 +6,23 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        Console.WriteLine("Give the first string:");
 
+            string String1 = Console.ReadLine();
+
+        Console.WriteLine("Give the second string");
+
+            string String2 = Console.ReadLine();
+
+            if (String1 == String2)
+            {
+                Console.WriteLine("Echo!");
+            }
+            else
+            {
+                Console.WriteLine("Nope!");
+            }
+            Console.ReadLine();
     }
   }
 }
