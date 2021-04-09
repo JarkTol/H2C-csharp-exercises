7118c79da9c0a4985473e4070954f398c5a57bcc
JarkTol <jarkko.tolli@centria.fi>
2021-04-09 17:11:48 +0300

    9.4.2021

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,29 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      while (true)
+      {
+                Console.WriteLine("Give a number");
+                int Number = Convert.ToInt32(Console.ReadLine());
 
+                if (Number > 0)
+                {
+                    Number = Number * Number;
+                    Console.WriteLine(Number);
+                }
+                
+                else if (Number <0)
+                {
+                    Console.WriteLine("That is negative");
+                }
+                
+                else if (Number == 0)
+                {
+                    break;
+                }
+
+      }
+      Console.ReadLine();
     }
   }
 }
