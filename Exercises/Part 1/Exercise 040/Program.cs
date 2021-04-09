9148b9ba68c6627b168a172915d2f9b53c8d51ad
JarkTol <jarkko.tolli@centria.fi>
2021-04-09 20:34:12 +0300

    9.4.2021

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -6,8 +6,28 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
+    int Count = 0;
+    int NumCount = 0;
+            
+            while (true)
+            {
+                Console.WriteLine("Give a number:");
+                int Number = Convert.ToInt32(Console.ReadLine());
+                
+                if (Number != 0)
+                {                       
+                    Count = Count + Number;
+                    NumCount = NumCount + 1;
+                }
 
-      // Write your code here:
+                else
+                {
+                   Console.WriteLine("Total sum of numbers: " + Count);
+                   Console.WriteLine("Total amount of number:" + NumCount);
+                    break;
+                }
+            }
+      Console.ReadLine();
     }
   }
 }
