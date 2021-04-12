6db06b76fbece3922793dcde07202d718d218aa6
JarkTol <jarkko.tolli@centria.fi>
2021-04-12 14:37:08 +0300

    12.4.2021

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -6,8 +6,21 @@ namespace exercise_45
   {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Give a number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for (int Count = 0; Count <= Number; Count++)
+                Console.WriteLine(Count);
+        
+        Console.WriteLine("Give a number");
+            int While = Convert.ToInt32(Console.ReadLine());
+            int Count2 = 0;
+            while (Count2 <= While)
+            {
+                Console.WriteLine(Count2);
+                Count2 = Count2 + 1;
+            }
+            Console.ReadLine();
 
-      // Write your code here:
 
     }
   }
