076fe39f959dc9853fe2375edda3d1181f40d180
JarkTol <jarkko.tolli@centria.fi>
2021-04-12 13:48:06 +0300

    12.4.2021

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -6,9 +6,15 @@ namespace exercise_42
   {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Input number");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input another number");
+        int Number2 = Convert.ToInt32(Console.ReadLine());
+            
+            double Square = Math.Sqrt(Number + Number2);
+            Console.WriteLine(Square);
+            Console.ReadLine();
 
-      // Write your code here:
-    
     }
   }
 }
