989110eb5eb234d54a4ea7b140a5fe3653c65d50
JarkTol <jarkko.tolli@centria.fi>
2021-04-12 13:53:37 +0300

    12.4.2021

--- Exercises/Part 2/Exercise 043/Program.cs
+++ Exercises/Part 2/Exercise 043/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        Console.WriteLine("Input number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number < 0)
+            {
+                Number = Number * -1;
+                Console.WriteLine(Number);
+            }
+            else
+                Console.WriteLine(Number);
+            Console.ReadLine();
       
     }
   }
