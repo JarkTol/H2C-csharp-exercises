a6de365c18d87ca34b71a4f0545228b8fab4f830
JarkTol <jarkko.tolli@centria.fi>
2021-05-06 16:38:25 +0300

    6.5.2021

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,18 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+        
+            PrintUntilNumber(100);
+            Console.ReadLine();
     }
 
     // Write your method here:
-
+    public static void PrintUntilNumber(int number)
+        {
+            for (int count = 1; count <= number; count++)
+            {
+                Console.WriteLine(count);
+            }
+        } 
   }
 }
