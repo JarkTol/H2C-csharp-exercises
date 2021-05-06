9607e37d9f6ea481404a759431dd69ec910a9c41
JarkTol <jarkko.tolli@centria.fi>
2021-05-06 14:08:40 +0300

    6.5.2021

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -7,12 +7,23 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+            
+            int times = Convert.ToInt32(Console.ReadLine());
+            
+            for (int count = 0; count <= times; count++)
+            {
+                PrintPhrase();            
+            }
+                
+            Console.ReadKey(); 
+    
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
-    }
+    public static void PrintPhrase() 
+        {
+            Console.WriteLine("In a hole in the ground there lived a method");
+        }
   }
 }
