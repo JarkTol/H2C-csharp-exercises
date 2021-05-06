8954a98e418a4ae0f0a7d95ea2a8e602770191e0
JarkTol <jarkko.tolli@centria.fi>
2021-05-06 17:30:25 +0300

    6.5.2021

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -7,11 +7,17 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      Division(34, 13);
+            Console.ReadLine();
 
     }
 
     // Write your method here:
+    public static void Division(int numerator, int denominator)
+        {
+            double result = Convert.ToDouble(numerator / denominator);
+            Console.WriteLine(result);
+        }
 
   }
 }
